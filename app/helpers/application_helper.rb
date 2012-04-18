07c0443a21aa7a40ed101e3a6be4d0d3934a2d1b
michael <michaelrkn@gmail.com>
2012-04-18 16:14:26 -0700

    Added Dynamic Naming

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title (title)
+    short_title = "Ruby on Rails Tutorial Sample App"
+    if title == "Sample App"  # the default title for the home page
+      short_title
+    else
+      return short_title + " | " + title
+    end
+  end
+  
 end
