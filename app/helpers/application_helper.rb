779fc6d4c44ccc76050b1cc1f331283556f01211
Brick Thornton <brickthornton@gmail.com>
2012-04-20 14:48:12 -0700

    Added CSS and Nav elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def full_title (title)
     short_title = "Ruby on Rails Tutorial Sample App"
-    if title == "Sample App"  # the default title for the home page
+    if title.nil?  # the default title for the home page
       short_title
     else
       return short_title + " | " + title
